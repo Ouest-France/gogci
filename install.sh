f3efdcc0796af8634df6b5e508c641c66950e096
Pablo RUTH <contact@pablo-ruth.fr>
2020-02-18 15:00:51 +0100

    add install script + readme to use it

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -O /bin/gogci https://github.com/Ouest-France/gogci/releases/latest/download/gogci
+wget -qO /bin/gogci https://github.com/Ouest-France/gogci/releases/latest/download/gogci
 chmod +x /bin/gogci
